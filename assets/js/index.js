d07da75ff33029747d4fd5caed60bdf77a521736
Mitchell Henke <mitchell@mitchellhenke.com>
2023-10-29 13:35:44 -0500

    blah

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,42 @@
 import Chart from 'chart.js/auto'
 
 (async function() {
-  const response = await fetch("/vision_zero_dashboard/data/summary/ytd_summary.json");
+  const response = await fetch("/data/summary/ytd_summary.json");
   const data = await response.json();
   const years = Object.keys(data);
   const fatalities = years.map(year => data[year].total_fatalities)
   const severe_injuries = years.map(year => data[year].total_severe_injuries)
 
    new Chart(
-    document.getElementById('chart'),
+    document.getElementById('fatalities-chart'),
     {
       type: 'line',
       options: {
         animation: false,
+        backgroundColor: 'rgb(22, 46, 81)',
+        borderColor: 'rgb(22, 46, 81)',
+        pointBackgroundColor: 'rgb(22, 46, 81)',
+        pointBorderColor: 'rgb(22, 46, 81)',
         plugins: {
           legend: {
             display: false
           },
           tooltip: {
-            enabled: false
+            enabled: true
+          },
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Year'
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Fatalities'
+            }
           }
         }
       },
@@ -27,7 +45,47 @@ import Chart from 'chart.js/auto'
         datasets: [
           {
             data: fatalities,
+          }
+        ]
+      }
+    }
+  );
+   new Chart(
+    document.getElementById('serious-injuries-chart'),
+    {
+      type: 'line',
+      options: {
+        animation: false,
+        backgroundColor: 'rgb(181, 9, 9)',
+        borderColor: 'rgb(181, 9, 9)',
+        pointBackgroundColor: 'rgb(181, 9, 9)',
+        pointBorderColor: 'rgb(181, 9, 9)',
+        plugins: {
+          legend: {
+            display: false
           },
+          tooltip: {
+            enabled: true
+          },
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Year'
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Serious Injuries'
+            }
+          }
+        }
+      },
+      data: {
+        labels: years,
+        datasets: [
           {
             data: severe_injuries,
           }
